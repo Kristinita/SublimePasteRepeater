524696f0d6bc5d9eebdfa653ad13bff6f1d2fdd6
Саша Черных <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-03-05 20:26:30 +0300

    [Fixed] Long text in clipboard

--- PasteRepeater.py
+++ PasteRepeater.py
@@ -3,7 +3,7 @@ import sublime, sublime_plugin
 class RepeaterCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     clip = sublime.get_clipboard();
-    self.view.window().show_input_panel("(%s)Paste how many times?" % clip, "", self.on_done, None, None)
+    self.view.window().show_input_panel("Paste how many times?", "", self.on_done, None, None)
 
   def on_done(self, user_input):
     pasteCount = int(float(user_input))
